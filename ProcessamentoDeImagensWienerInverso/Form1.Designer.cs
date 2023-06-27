006ae57c0d37fcb169489e68816a96a86e338af9
Urias <48036833+XuriMechanics@users.noreply.github.com>
2023-06-26 22:07:57 -0300

    Add files via upload

---
+++ ProcessamentoDeImagensWienerInverso/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ProcessamentoDeImagensWienerInverso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btLoadA = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleDescription = "";
+            this.pictureBox1.AccessibleName = "";
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(714, 61);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btLoadA
+            // 
+            this.btLoadA.BackColor = System.Drawing.SystemColors.Window;
+            this.btLoadA.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btLoadA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLoadA.Location = new System.Drawing.Point(189, 599);
+            this.btLoadA.Name = "btLoadA";
+            this.btLoadA.Size = new System.Drawing.Size(146, 32);
+            this.btLoadA.TabIndex = 2;
+            this.btLoadA.Text = "Carregar Imagem";
+            this.btLoadA.UseVisualStyleBackColor = false;
+            this.btLoadA.Click += new System.EventHandler(this.btLoadA_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(549, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Filtragem Inversa";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(886, 599);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Salvar Imagem";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Imagem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(711, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Imagem Resultante";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1247, 730);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btLoadA);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btLoadA;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ProcessamentoDeImagensWienerInverso/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ProcessamentoDeImagensWienerInverso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btLoadA_Click(object sender, EventArgs e)
+        {
+
+            Bitmap img1 = null;
+            byte[,] vImg1Gray;
+            byte[,] vImg1R;
+            byte[,] vImg1G;
+            byte[,] vImg1B;
+            byte[,] vImg1A;
+
+
+            // Configurações iniciais da OpenFileDialogBox
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Armnazena o path do arquivo de imagem
+                filePath = openFileDialog1.FileName;
+
+                bool bLoadImgOK = false;
+                try
+                {
+                    img1 = new Bitmap(filePath);
+                    bLoadImgOK = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+
+                // Se a imagem carregou perfeitamente...
+                if (bLoadImgOK == true)
+                {
+                    // Adiciona imagem na PictureBox
+                    pictureBox1.Image = img1;
+                    vImg1Gray = new byte[img1.Width, img1.Height];
+                    vImg1R = new byte[img1.Width, img1.Height];
+                    vImg1G = new byte[img1.Width, img1.Height];
+                    vImg1B = new byte[img1.Width, img1.Height];
+                    vImg1A = new byte[img1.Width, img1.Height];
+
+                    // Percorre todos os pixels da imagem...
+                    for (int i = 0; i < img1.Width; i++)
+                    {
+                        for (int j = 0; j < img1.Height; j++)
+                        {
+                            Color pixel = img1.GetPixel(i, j);
+
+                            // Para imagens em escala de cinza, extrair o valor do pixel com...
+                            //byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                            byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                            vImg1Gray[i, j] = pixelIntensity;
+
+                            // Para imagens RGB, extrair o valor do pixel com...
+                            byte R = pixel.R;
+                            byte G = pixel.G;
+                            byte B = pixel.B;
+                            byte A = pixel.A;
+
+                            vImg1R[i, j] = R;
+                            vImg1G[i, j] = G;
+                            vImg1B[i, j] = B;
+                            vImg1A[i, j] = A;
+
+                        }
+                    }
+                }
+            }
+
+   
+
+    }
+
+    }
+}
+
